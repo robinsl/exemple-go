838d0624d9ccb192ee0ce72cb4963edb91fb8b42
Robin <robinlamarre@gmail.com>
2023-08-13 14:24:26 -0400

    change tabs to 4

--- internal/book/Response.go
+++ internal/book/Response.go
@@ -1,40 +1,40 @@
 package book
 
 import (
-    "github.com/go-chi/render"
-    "github.com/google/uuid"
-    "net/http"
+	"github.com/go-chi/render"
+	"github.com/google/uuid"
+	"net/http"
 )
 
 type bookResponse struct {
-    ID          uuid.UUID `json:"id"`
-    Title       string    `json:"title"`
-    Description string    `json:"description"`
-    Page        int       `json:"page"`
-    CreatedAt   string    `json:"created_at"`
-    UpdatedAt   string    `json:"updated_at"`
+	ID          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Page        int       `json:"page"`
+	CreatedAt   string    `json:"created_at"`
+	UpdatedAt   string    `json:"updated_at"`
 }
 
 func NewBookResponse(book Book) bookResponse {
-    return bookResponse{
-        ID:          book.ID,
-        Title:       book.Title,
-        Description: book.Description,
-        Page:        book.Page,
-        CreatedAt:   book.CreatedAt.Format("2006-01-02 15:04:05"),
-        UpdatedAt:   book.UpdatedAt.Format("2006-01-02 15:04:05"),
-    }
+	return bookResponse{
+		ID:          book.ID,
+		Title:       book.Title,
+		Description: book.Description,
+		Page:        book.Page,
+		CreatedAt:   book.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt:   book.UpdatedAt.Format("2006-01-02 15:04:05"),
+	}
 }
 
 func (response bookResponse) Render(_ http.ResponseWriter, _ *http.Request) error {
-    return nil
+	return nil
 }
 
 func NewBookListResponse(books []Book) []render.Renderer {
-    list := []render.Renderer{}
-    for _, book := range books {
-        list = append(list, NewBookResponse(book))
-    }
+	list := []render.Renderer{}
+	for _, book := range books {
+		list = append(list, NewBookResponse(book))
+	}
 
-    return list
+	return list
 }
